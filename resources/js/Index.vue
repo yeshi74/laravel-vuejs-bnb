ee5b089f921d1cec8fdda21b90e22496eb996e86
Yeshi <thebluediamond96@gmail.com>
2022-01-06 20:18:33 +0530

    navbar style

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        <router-link :to="{name: 'home'}">Home</router-link>
-        <router-link :to="{name: 'example2'}">Example2</router-link>
+        <nav class="navbar bg-white border-bottom navbar-light">
+        <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">LaravelBnb</router-link>
+        <router-link class="btn nav-button" :to="{name: 'example2'}">Example2</router-link>
+        </nav>
+
+        <div class="container mt-4 mb-4 pr-4 pl-4">
         <router-view></router-view>
+        </div>
+
     </div>
 </template>
