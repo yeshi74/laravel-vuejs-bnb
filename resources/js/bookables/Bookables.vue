7dafbe699e21630180d9aed562105b5d6e8adef7
Yeshi <thebluediamond96@gmail.com>
2022-01-07 14:33:41 +0530

    List rendering & v-else

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,18 +1,14 @@
 <template>
     <div>
-        <bookable-list-item
-        v-if="bookable1"
-            :item-title="bookable1.title"
-            :item-content="bookable1.content"
-            :price="1000">
-        </bookable-list-item>
-
-        <bookable-list-item
-        v-if="bookable2"
-            :item-title="bookable2.title"
-            :item-content="bookable2.content"
-            :price="1500">
-        </bookable-list-item>
+        <div v-if="loading">Data is loading...</div>
+        <div v-else>
+            <bookable-list-item
+            :item-title="bookable.title"
+            :item-content="bookable.content"
+            :price="1000"
+            v-for="(bookable, index) in bookables" :key="index"
+        ></bookable-list-item>
+        </div>
 
     </div>
 </template>
@@ -26,22 +22,24 @@ export default {
 
     data() {
         return {
-            bookable1: null,
-            bookable2: null,
+            bookables: null,
+            loading: false
         };
     },
 
     created() {
+        this.loading = true;
 
         setTimeout(() => {
-            this.bookable1 = {
+            this.bookables = [{
                 title: 'Cheap Villa!!',
                 content: 'Very cheap Villa'
-        };
-        this.bookable2 = {
+            },
+            {
                 title: 'Cheap Villa 2!!',
                 content: 'Very cheap Villa 2'
-        };
+            }];
+            this.loading = false;
         }, 2000);
     }
 };
