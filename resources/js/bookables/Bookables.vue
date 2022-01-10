5175fa36bccf743058ffa58b486ac190517d71bf
Yeshi <thebluediamond96@gmail.com>
2022-01-10 12:40:14 +0530

    component methods

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
-        Rows is: {{ rows }}
         <div v-if="loading">Data is loading...</div>
         <div v-else>
-            <bookable-list-item
-            :item-title="bookable.title"
-            :item-content="bookable.content"
-            :price="1000"
-            v-for="(bookable, index) in bookables" :key="index"
-        ></bookable-list-item>
+            <div class="row mb-4" v-for="row in rows" :key="'row' + row">
+                <div class="col" v-for="(bookable, column) in bookablesInRow(row)" :key="'row' + row + column">
+                    <bookable-list-item
+                        :item-title="bookable.title"
+                        :item-content="bookable.content"
+                        :price="1000"
+                    ></bookable-list-item>
+                </div>
+                <div class="col" v-for="p in placeholdersInRow(row)" :key="'placeholder' + row + p"></div>
+            </div>
+
         </div>
 
     </div>
@@ -33,6 +37,15 @@ export default {
         rows() {
             return this.bookables == null ? 0 : Math.ceil(this.bookables.length / this.columns)
         }
+    },
+
+    methods: {
+        bookablesInRow(row) {
+            return this.bookables.slice((row - 1) * this.columns, row * this.columns);
+        },
+        placeholdersInRow(row) {
+        return this.columns - this.bookablesInRow(row).length;
+    },
     },
 
     created() {
