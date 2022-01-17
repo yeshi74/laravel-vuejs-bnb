507ddcc626a090e1eb535726d9b10e8b199c9cff
Yeshi <thebluediamond96@gmail.com>
2022-01-17 14:01:39 +0530

    eloquent query scope

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
-
+use App\Models\Bookable;
 use Illuminate\Http\Request;
 
 class BookableAvailabilityController extends Controller
@@ -13,11 +13,14 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
         $data = $request->validate([
             'from' => 'required|date_format:Y-m-d|after_or_equal:now',
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
         ]);
+
+        $bookable = Bookable::findOrFail($id);
+        dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
     }
 }
