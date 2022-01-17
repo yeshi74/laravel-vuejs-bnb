310b4c90bd774746c03ff026eb7f8cc74e2c87e2
Yeshi <thebluediamond96@gmail.com>
2022-01-17 11:12:30 +0530

    Handling DOM events in vue

--- public/js/app.js
+++ public/js/app.js
@@ -5292,12 +5292,19 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   data: function data() {
     return {
       from: null,
       to: null
     };
+  },
+  methods: {
+    check: function check() {
+      alert('I will check something now!');
+    }
   }
 });
 
@@ -28865,6 +28872,15 @@ var render = function () {
           attrs: { type: "text", name: "from", placeholder: "Start date" },
           domProps: { value: _vm.from },
           on: {
+            keyup: function ($event) {
+              if (
+                !$event.type.indexOf("key") &&
+                _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+              ) {
+                return null
+              }
+              return _vm.check.apply(null, arguments)
+            },
             input: function ($event) {
               if ($event.target.composing) {
                 return
@@ -28891,6 +28907,15 @@ var render = function () {
           attrs: { type: "text", name: "to", placeholder: "End date" },
           domProps: { value: _vm.to },
           on: {
+            keyup: function ($event) {
+              if (
+                !$event.type.indexOf("key") &&
+                _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+              ) {
+                return null
+              }
+              return _vm.check.apply(null, arguments)
+            },
             input: function ($event) {
               if ($event.target.composing) {
                 return
@@ -28902,9 +28927,11 @@ var render = function () {
       ]),
     ]),
     _vm._v(" "),
-    _c("button", { staticClass: "btn btn-secondary btn-block" }, [
-      _vm._v("Check!"),
-    ]),
+    _c(
+      "button",
+      { staticClass: "btn btn-secondary btn-block", on: { click: _vm.check } },
+      [_vm._v("Check!")]
+    ),
   ])
 }
 var staticRenderFns = []
--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -10,7 +10,8 @@
                 name="from"
                 class="form-control form-control-sm"
                 placeholder="Start date"
-                v-model="from">
+                v-model="from"
+                @keyup.enter="check">
             </div>
 
             <div class="form-group col-md-6">
@@ -20,11 +21,12 @@
                 name="to"
                 class="form-control form-control-sm"
                 placeholder="End date"
-                v-model="to">
+                v-model="to"
+                @keyup.enter="check">
             </div>
 
         </div>
-        <button class="btn btn-secondary btn-block">Check!</button>
+        <button class="btn btn-secondary btn-block" @click="check">Check!</button>
     </div>
 </template>
 
@@ -34,6 +36,11 @@ export default {
         return {
             from: null,
             to: null
+        };
+    },
+    methods: {
+        check() {
+            alert('I will check something now!');
         }
     }
 }
